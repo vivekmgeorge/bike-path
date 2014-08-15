851a0a2425c7dd28005f272b0f3b3f397c673c5c
Farheen Malik <farheen.m.malik@gmail.com>
2014-08-14 21:35:47 -0400

    Add View controller for Search Map

---
+++ SearchMap.h
@@ -0,0 +1,13 @@
+//
+//  SearchMap.h
+//  bikepath
+//
+//  Created by Farheen Malik on 8/14/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchMap : UIViewController
+
+@end
