fbf2ec90506dfc0d8f9ac182ad58cd0c29016337
Farheen Malik <farheen.m.malik@gmail.com>
2014-08-16 12:55:21 -0400

    Modify initial page to just show map, add table view page

---
+++ bikepath/SearchListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchListViewController.h
+//  bikepath
+//
+//  Created by Farheen Malik on 8/16/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchListViewController : UIViewController
+
+@end
---
+++ bikepath/SearchListViewController.m
@@ -0,0 +1,13 @@
+//
+//  SearchListViewController.m
+//  bikepath
+//
+//  Created by Farheen Malik on 8/16/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import "SearchListViewController.h"
+
+@implementation SearchListViewController
+
+@end
