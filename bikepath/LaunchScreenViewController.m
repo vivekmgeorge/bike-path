8316e8b9213fed274697c9d0d6cff7082b31e340
Vivek M George <null>
2014-08-16 16:23:54 -0400

    created some files for the initial launch screen

--- bikepath/LaunchScreenViewController.m
+++ bikepath/LaunchScreenViewController.m
@@ -7,7 +7,26 @@
 //
 
 #import "LaunchScreenViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
 @implementation LaunchScreenViewController
+
+- (void)startStandardUpdates
+{
+    CLLocationManager *locationManager;
+    
+    // Create the location manager if this object does not
+    // already have one.
+    if (nil == locationManager)
+        locationManager = [[CLLocationManager alloc] init];
+    
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+    
+    // Set a movement threshold for new events.
+    locationManager.distanceFilter = 500; // meters
+    
+    [locationManager startUpdatingLocation];
+}
 
 @end
