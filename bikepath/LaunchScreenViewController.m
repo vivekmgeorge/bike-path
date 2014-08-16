9fe3b4c3eaf8dd1eef2b90cc0dc93a1f924758ba
Vivek M George <null>
2014-08-16 16:23:41 -0400

    created some files for the initial launch screen

---
+++ bikepath/LaunchScreenViewController.m
@@ -0,0 +1,13 @@
+//
+//  LaunchScreenViewController.m
+//  bikepath
+//
+//  Created by Apprentice on 8/16/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import "LaunchScreenViewController.h"
+
+@implementation LaunchScreenViewController
+
+@end
