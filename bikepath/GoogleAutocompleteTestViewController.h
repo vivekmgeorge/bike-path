61f3914c891a15aeee47a08c6a8f4317123af50c
Farheen Malik <farheen.m.malik@gmail.com>
2014-08-17 18:14:06 -0400

    Add Google Autocomplete Test view Controllers

---
+++ bikepath/GoogleAutocompleteTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  GoogleAutocompleteTestViewController.h
+//  bikepath
+//
+//  Created by Apprentice on 8/17/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoogleAutocompleteTestViewController : UIViewController
+
+@end
---
+++ bikepath/GoogleAutocompleteTestViewController.m
@@ -0,0 +1,49 @@
+//
+//  GoogleAutocompleteTestViewController.m
+//  bikepath
+//
+//  Created by Apprentice on 8/17/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import "GoogleAutocompleteTestViewController.h"
+
+@interface GoogleAutocompleteTestViewController ()
+
+@end
+
+@implementation GoogleAutocompleteTestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
