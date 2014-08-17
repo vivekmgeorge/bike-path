70da9d67fa167742f26c3a954ca75c7e9db83d63
Armen Vartan <vartan.armen@gmail.com>
2014-08-17 06:59:48 -0400

    cleaning up DirectionService

--- bikepath/MDDirectionService.m
+++ bikepath/MDDirectionService.m
@@ -18,8 +18,7 @@
 
 static NSString *kMDDirectionsURL = @"http://maps.googleapis.com/maps/api/directions/json?mode=bicycling";
 
-- (void)setDirectionsQuery:(NSDictionary *)query withSelector:(SEL)selector
-              withDelegate:(id)delegate{
+- (void)setDirectionsQuery:(NSDictionary *)query withSelector:(SEL)selector withDelegate:(id)delegate{
     NSArray *waypoints = [query objectForKey:@"waypoints"];
     NSString *origin = [waypoints objectAtIndex:0];
     int waypointCount = [waypoints count];
@@ -27,14 +26,14 @@ static NSString *kMDDirectionsURL = @"http://maps.googleapis.com/maps/api/direct
     NSString *destination = [waypoints objectAtIndex:destinationPos];
     NSString *sensor = [query objectForKey:@"sensor"];
     NSMutableString *url = [NSMutableString stringWithFormat:@"%@&origin=%@&destination=%@&sensor=%@",
-     kMDDirectionsURL, origin, destination, sensor];
+                            kMDDirectionsURL, origin, destination, sensor];
+    
     if(waypointCount>2) {
         [url appendString: @"&waypoints="];
         int wpCount = waypointCount-1;
         for(int i=1;i<wpCount;i++){
             [url appendString: @"|"];
             [url appendString:[waypoints objectAtIndex:i]];
-                NSLog(@"hello %@", url);
         }
     }
     url = [url stringByAddingPercentEscapesUsingEncoding: NSASCIIStringEncoding];
