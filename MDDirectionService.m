6b3585185f21326fb5714de410e3e74eb66fc1ef
Armen Vartan <vartan.armen@gmail.com>
2014-08-17 16:08:42 -0400

    changed DestinationService to take waypoints index1 as destination

--- MDDirectionService.m
+++ MDDirectionService.m
@@ -22,16 +22,16 @@ static NSString *kMDDirectionsURL = @"http://maps.googleapis.com/maps/api/direct
     NSArray *waypoints = [query objectForKey:@"waypoints"];
     NSString *origin = [waypoints objectAtIndex:0];
     int waypointCount = [waypoints count];
-    int destinationPos = waypointCount -1;
-    NSString *destination = [waypoints objectAtIndex:destinationPos];
+//    int destinationPos = [waypoints objectAtIndex:1];
+    NSString *destination = [waypoints objectAtIndex:1];
     NSString *sensor = [query objectForKey:@"sensor"];
     NSMutableString *url = [NSMutableString stringWithFormat:@"%@&origin=%@&destination=%@&sensor=%@",
                             kMDDirectionsURL, origin, destination, sensor];
     
     if(waypointCount>2) {
         [url appendString: @"&waypoints="];
-        int wpCount = waypointCount-1;
-        for(int i=1;i<wpCount;i++){
+        int wpCount = waypointCount;
+        for(int i=2;i<wpCount;i++){
             [url appendString: @"|"];
             [url appendString:[waypoints objectAtIndex:i]];
         }
