d67f826d2c84c4ed5bc5e81a259f11d0de118782
Armen Vartan <vartan.armen@gmail.com>
2014-08-17 11:19:40 -0400

    probably slight refactorings. mving to another commit to play

--- bikepath/ResultsMapViewController.m
+++ bikepath/ResultsMapViewController.m
@@ -30,7 +30,14 @@
     
     waypoints_ = [[NSMutableArray alloc]init];
     waypointStrings_ = [[NSMutableArray alloc]init];
-
+    
+    self.mapView.mapType = kGMSTypeNormal;
+    self.mapView.myLocationEnabled          = YES;
+    self.mapView.settings.compassButton     = YES;
+    self.mapView.settings.myLocationButton  = YES;
+    self.mapView.settings.zoomGestures      = YES;
+    self.mapView.delegate                   = self;
+    
     
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:40.706638
                                                             longitude:-74.009070
@@ -48,14 +55,81 @@
     NSString *startPositionString = [[NSString alloc] initWithFormat:@"%f,%f", 40.706638, -74.009070];
     [waypointStrings_ addObject:startPositionString];
     
-    CLLocationCoordinate2D startStationPosition = CLLocationCoordinate2DMake(40.705638, -74.013070);
-    GMSMarker *startStationPoint = [GMSMarker markerWithPosition:startStationPosition];
-    startStationPoint.title = @"Start station";
-    startStationPoint.map = mapView_;
-    [waypoints_ addObject:startStationPoint];
+    NSURL *url = [NSURL URLWithString:@"http://www.citibikenyc.com/stations/json"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response,
+                                               NSData *data, NSError *connectionError)
+     {
+         if (data.length > 0 && connectionError == nil)
+         {
+             NSDictionary *greeting = [NSJSONSerialization JSONObjectWithData:data
+                                                                      options:0
+                                                                        error:NULL];
+             NSArray *stations = [greeting objectForKey:@"stationBeanList"];
+             CLLocationDistance smallestDistance = DBL_MAX;
+             CLLocation *closestLocation;
+             NSDictionary *closestStation;
+             
+             for(id st in stations) {
+                 NSDictionary *station      = (NSDictionary *)st;
+                 NSString *stationLatitude  = [station objectForKey:@"latitude"];
+                 NSString *stationLongitude = [station objectForKey:@"longitude"];
+                 
+                 CLLocation *bikeStop = [[CLLocation alloc] initWithLatitude:[stationLatitude doubleValue] longitude:[stationLongitude doubleValue]];
+                 CLLocation *currentLocation = self.mapView.myLocation;
+                 
+                 NSMutableArray *locations = [[NSMutableArray alloc] init];
+                 [locations addObject:bikeStop];
+                 
+                 for (CLLocation *location in locations) {
+                     CLLocationDistance distance = [currentLocation distanceFromLocation:location];
+                     
+                     if (distance < smallestDistance) {
+                         smallestDistance    = distance;
+                         closestLocation     = location;
+                         closestStation      = station;
+                     }
+                 }
+                 
+             }
+             NSLog(@"%@", closestLocation);
+             NSLog(@"%f", closestLocation.coordinate.longitude);
+             
+             NSString *title            = [closestStation objectForKey:@"stationName"];
+             NSString *availableBikes   = [[closestStation objectForKey:@"availableBikes"] stringValue];
+             NSNumber *numBikes         = @([[closestStation objectForKey:@"availableBikes"] intValue]);
+             
+             GMSMarker *citiMarker  = [[GMSMarker alloc] init];
+             
+             if ([numBikes intValue] > 0) {
+                 citiMarker.icon    = [GMSMarker markerImageWithColor:[UIColor greenColor]];
+                 citiMarker.snippet = availableBikes;
+             } else {
+                 citiMarker.icon    = [GMSMarker markerImageWithColor:[UIColor redColor]];
+                 citiMarker.snippet = @"No bikes available at this location.";
+             };
+             
+             citiMarker.title       = title;
+             citiMarker.position    = closestLocation.coordinate;
+             citiMarker.map         = self.mapView;
+             NSLog(@"%@", citiMarker);
+             
+             [waypoints_ addObject:citiMarker];
+             NSString *citiMarkerString = [[NSString alloc] initWithFormat:@"%f,%f", closestLocation.coordinate.latitude, closestLocation.coordinate.longitude];
+             [waypointStrings_ addObject:citiMarkerString];
+         }
+     }];
     
-    NSString *startStationPositionString = [[NSString alloc] initWithFormat:@"%f,%f", 40.705638, -74.013070];
-    [waypointStrings_ addObject:startStationPositionString];
+//    CLLocationCoordinate2D startStationPosition = CLLocationCoordinate2DMake(40.705638, -74.013070);
+//    GMSMarker *startStationPoint = [GMSMarker markerWithPosition:startStationPosition];
+//    startStationPoint.title = @"Start station";
+//    startStationPoint.map = mapView_;
+//    [waypoints_ addObject:startStationPoint];
+//    
+//    NSString *startStationPositionString = [[NSString alloc] initWithFormat:@"%f,%f", 40.705638, -74.013070];
+//    [waypointStrings_ addObject:startStationPositionString];
     
     CLLocationCoordinate2D endStationPosition = CLLocationCoordinate2DMake(40.722638, -74.009070);
     GMSMarker *endStationPoint = [GMSMarker markerWithPosition:endStationPosition];
@@ -74,7 +148,6 @@
     
     NSString *endPositionString = [[NSString alloc] initWithFormat:@"%f,%f", 40.720638, -74.006070];
     [waypointStrings_ addObject:endPositionString];
-    NSLog(@"%@", waypointStrings_);
     
     NSString *sensor = @"false";
     NSArray *parameters = [NSArray arrayWithObjects:sensor, waypointStrings_,
