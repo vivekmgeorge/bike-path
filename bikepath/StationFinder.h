310ae99be6678622b9c7a095d72204c822da42f7
Vivek M George <vivek.m.george@gmail.com>
2014-08-17 21:49:21 -0400

    Added a hard copy of our json feed to do testing

---
+++ bikepath/StationFinder.h
@@ -0,0 +1,13 @@
+//
+//  StationFinder.h
+//  bikepath
+//
+//  Created by Vivek M George on 8/17/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StationFinder : NSObject
+
+@end
---
+++ bikepath/StationFinder.m
@@ -0,0 +1,13 @@
+//
+//  StationFinder.m
+//  bikepath
+//
+//  Created by Vivek M George on 8/17/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import "StationFinder.h"
+
+@implementation StationFinder
+
+@end
