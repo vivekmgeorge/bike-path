77f903071646349416e884e5b0e501a78517ef6b
Vivek M George <vivek.m.george@gmail.com>
2014-08-17 22:01:06 -0400

    This is the new class method to find the nearest station

--- bikepath/StationFinder.m
+++ bikepath/StationFinder.m
@@ -7,7 +7,48 @@
 //
 
 #import "StationFinder.h"
+#import <CoreLocation/CoreLocation.h>
 
 @implementation StationFinder
+
++ (NSDictionary *) findClosestStation: (NSArray *) stations
+                   location: (CLLocation *) currentLocation {
+
+    
+    CLLocationDistance smallestDistance = DBL_MAX;
+//    CLLocation *closestLocation;
+    NSDictionary *closestStation;
+    
+    //        CLLocation *closestEndLocation;
+    //        NSDictionary *closestEndStation;
+    
+    for(id st in stations) {
+        NSDictionary *station      = (NSDictionary *)st;
+        NSString *stationLatitude  = [station objectForKey:@"latitude"];
+        NSString *stationLongitude = [station objectForKey:@"longitude"];
+        
+        
+        CLLocation *bikeStop = [[CLLocation alloc] initWithLatitude:[stationLatitude doubleValue] longitude:[stationLongitude doubleValue]];
+        
+        NSMutableArray *locations = [[NSMutableArray alloc] init];
+        [locations addObject:bikeStop];
+        
+        for (CLLocation *location in locations) {
+            CLLocationDistance distance = [currentLocation distanceFromLocation:location];
+            
+            if (distance < smallestDistance) {
+                smallestDistance    = distance;
+//                closestLocation     = location;
+                closestStation      = station;
+            }
+        }
+        
+    }
+    
+    //        print it
+//    NSLog(@"%@", closestStation);
+
+    return closestStation;
+}
 
 @end
