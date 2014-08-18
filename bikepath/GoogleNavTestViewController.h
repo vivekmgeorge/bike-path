32165f5f6afbe6d8ee7c51205995f8d91af2e041
Farheen Malik <farheen.m.malik@gmail.com>
2014-08-18 18:41:09 -0400

    Modify google nav test controller to include button that leads to open Google app

--- bikepath/GoogleNavTestViewController.h
+++ bikepath/GoogleNavTestViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface GoogleNavTestView_Controller : UIViewController
+@interface GoogleNavTestViewController : UIViewController
 
 @end
--- bikepath/GoogleNavTestViewController.m
+++ bikepath/GoogleNavTestViewController.m
@@ -6,8 +6,58 @@
 //  Copyright (c) 2014 Bike Path. All rights reserved.
 //
 
-#import "GoogleNavTestView Controller.h"
+#import "GoogleNavTestViewController.h"
 
-@implementation GoogleNavTestView_Controller
+@implementation GoogleNavTestViewController
 
+//@synthesize launchNav;
+
+- (void)loadView {
+    
+    //allocate the view
+    self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
+    
+    //set the view's background color
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    //create the button
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    //set the position of the button
+    button.frame = CGRectMake(100, 170, 100, 30);
+    
+    //set the button's title
+    [button setTitle:@"Click Me!" forState:UIControlStateNormal];
+    
+    //listen for clicks
+    [button addTarget:self action:@selector(buttonPressed)
+     forControlEvents:UIControlEventTouchUpInside];
+    
+    //add the button to the view
+    [self.view addSubview:button];
+}
+
+-(void)buttonPressed {
+    NSLog(@"Button Pressed!");
+    NSURL *testURL = [NSURL URLWithString:@"comgooglemaps-x-callback://"];
+    if ([[UIApplication sharedApplication] canOpenURL:testURL]) {
+        NSString *directionsRequest = @"comgooglemaps-x-callback://?daddr=John+F.+Kennedy+International+Airport,+Van+Wyck+Expressway,+Jamaica,+New+York&x-success=sourceapp://?resume=true&x-source=AirApp";
+        NSURL *directionsURL = [NSURL URLWithString:directionsRequest];
+        [[UIApplication sharedApplication] openURL:directionsURL];
+    } else {
+        NSLog(@"Can't use comgooglemaps-x-callback:// on this device.");
+    }
+}
+
+// https://developers.google.com/maps/documentation/ios/urlscheme#add_navigation_to_your_app
+//NSURL *testURL = [NSURL URLWithString:@"comgooglemaps-x-callback://"];
+//if ([[UIApplication sharedApplication] canOpenURL:testURL]) {
+//    NSString *directionsRequest = @"comgooglemaps-x-callback://" +
+//    @"?daddr=John+F.+Kennedy+International+Airport,+Van+Wyck+Expressway,+Jamaica,+New+York" +
+//    @"&x-success=sourceapp://?resume=true&x-source=AirApp";
+//    NSURL *directionsURL = [NSURL URLWithString:directionsRequest];
+//    [[UIApplication sharedApplication] openURL:directionsURL];
+//} else {
+//    NSLog(@"Can't use comgooglemaps-x-callback:// on this device.");
+//}
 @end
