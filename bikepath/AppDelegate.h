1d00f36479cb9a3cb6d30994a82a45a88a5f40da
mhuerster <mary.huerster@gmail.com>
2014-08-18 23:04:55 -0400

    create station object, add nsurlcache to appdelegate header not using station object yet - may need to remove this later

--- bikepath/AppDelegate.h
+++ bikepath/AppDelegate.h
@@ -11,6 +11,6 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-@property (strong, nonatomic) NSCache *citiBikeCache;
+@property (strong, nonatomic) NSURLCache *citiBikeCache;
 
 @end
---
+++ bikepath/CitiBikeStationsJSON.h
@@ -0,0 +1,13 @@
+//
+//  CitiBikeStationsJSON.h
+//  bikepath
+//
+//  Created by Molly Huerster on 8/18/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CitiBikeStationsJSON : NSObject
+
+@end
