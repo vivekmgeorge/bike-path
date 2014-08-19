860ac4cbca5f02e4729a18cbed13d1663c041f4e
mhuerster <mary.huerster@gmail.com>
2014-08-19 17:43:01 -0400

    add citibike view controller

---
+++ bikepath/CitiBikeController.m
@@ -0,0 +1,13 @@
+//
+//  CitiBikeController.m
+//  bikepath
+//
+//  Created by Armen Vartan on 8/19/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import "CitiBikeController.h"
+
+@implementation CitiBikeController
+
+@end
