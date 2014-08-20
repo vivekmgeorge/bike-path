7bb3a129036ce2d793da1c1438351c0d3667bd69
mhuerster <mary.huerster@gmail.com>
2014-08-20 11:42:29 -0400

    create error message factory

---
+++ bikepath/ErrorMessage.h
@@ -0,0 +1,18 @@
+//
+//  ErrorMessage.h
+//  bikepath
+//
+//  Created by Molly Huerster on 8/20/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ErrorMessage : NSObject
+
++ (UIAlertView*)renderErrorMessage:(NSString*) messageTitle
+                 cancelButtonTitle:(NSString*) buttonTitle
+                             error:(NSError*) error;
+
+
+@end
