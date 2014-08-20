375af1a883e38a717bd49e7b7e83b01fa40169e0
mhuerster <mary.huerster@gmail.com>
2014-08-20 17:25:43 -0400

    refactoring delete unused/redundant variables reformat/fix spacing

--- bikepath/SearchMapViewController.m
+++ bikepath/SearchMapViewController.m
@@ -28,14 +28,15 @@
     AppDelegate *appDel = (AppDelegate *)[[UIApplication sharedApplication] delegate];
     [appDel loadCitiBikeData];
   
-    GMSCameraPosition *dbc = [GMSCameraPosition cameraWithLatitude:40.706638
+    GMSCameraPosition *startLocation = [GMSCameraPosition cameraWithLatitude:40.706638
                                                          longitude:-74.009070
                                                               zoom:16];
     
-//    GMSCameraPosition *dbc = [GMSCameraPosition cameraWithLatitude:37.7848395
+//    GMSCameraPosition *startLocation = [GMSCameraPosition cameraWithLatitude:37.7848395
 //                                                            longitude:-122.4041945
 //                                                                 zoom:15];
-    [self.mapView setCamera:dbc];
+    
+    [self.mapView setCamera:startLocation];
     self.mapView.mapType                    = kGMSTypeNormal;
     self.mapView.myLocationEnabled          = YES;
     self.mapView.settings.compassButton     = YES;
@@ -43,35 +44,26 @@
     self.mapView.settings.zoomGestures      = YES;
     self.mapView.delegate                   = self;
     
-    NSArray *sortedStations = appDel.stationJSON;
-    
-    for(id st in sortedStations) {
-        NSDictionary *station = (NSDictionary *)st;
-        NSString *lati             = [station objectForKey:@"latitude"];
-        NSString *longi            = [station objectForKey:@"longitude"];
-        NSString *title            = [station objectForKey:@"stationName"];
-        NSString *availableBikes   = [[station objectForKey:@"availableBikes"] stringValue];
+    for(id station in appDel.stationJSON) {
+        NSString *latitude          = [station objectForKey:@"latitude"];
+        NSString *longitude         = [station objectForKey:@"longitude"];
+        NSString *title             = [station objectForKey:@"stationName"];
+        NSString *availableBikes    = [[station objectForKey:@"availableBikes"] stringValue];
+        NSNumber *num               = @([[station objectForKey:@"availableBikes"] intValue]);
         
-                 GMSMarker *citiMarker = [[GMSMarker alloc] init];
-                 
-                 citiMarker.position = CLLocationCoordinate2DMake([lati doubleValue], [longi doubleValue]);
-                 citiMarker.title    = title;
-                 citiMarker.map      = self.mapView;
-                 
-                 NSNumber *num = @([[station objectForKey:@"availableBikes"] intValue]);
-                 
-                 CLLocation *location = [[CLLocation alloc] initWithLatitude:[lati doubleValue] longitude:[longi doubleValue]];
-                 NSMutableArray *locations = [[NSMutableArray alloc] init];
-                 [locations addObject:location];
-                 
-                 if ([num intValue] > 0) {
-                     citiMarker.icon = [UIImage imageNamed:@"bicycleGreen"];
-                     citiMarker.snippet  = [NSString stringWithFormat:@"Bicyles avaiable: %@", availableBikes];
-                 } else {
-                     citiMarker.icon = [UIImage imageNamed:@"bicycleRed"];
-                     citiMarker.snippet = @"No bicyles availabe at this location.";
-                 };
-                 citiMarker.map = self.mapView;
+        GMSMarker *citiMarker   = [[GMSMarker alloc] init];
+        citiMarker.position     = CLLocationCoordinate2DMake([latitude doubleValue], [longitude doubleValue]);
+        citiMarker.title        = title;
+        citiMarker.map          = self.mapView;
+        
+        if ([num intValue] > 0) {
+            citiMarker.icon = [UIImage imageNamed:@"bicycleGreen"];
+            citiMarker.snippet  = [NSString stringWithFormat:@"Bicyles available: %@", availableBikes];
+        } else {
+            citiMarker.icon = [UIImage imageNamed:@"bicycleRed"];
+            citiMarker.snippet = @"No bicyles available at this location.";
+        };
+        citiMarker.map = self.mapView;
     }
 }
 
