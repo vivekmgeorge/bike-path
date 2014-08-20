ed559941a90b9dc307336e3b810b89671c978422
Farheen Malik <farheen.m.malik@gmail.com>
2014-08-20 00:33:00 -0400

    First shot at refactoring out geocoding call

---
+++ bikepath/GeocodeItem.h
@@ -0,0 +1,13 @@
+//
+//  GeocodeItem.h
+//  bikepath
+//
+//  Created by Farheen Malik on 8/19/14.
+//  Copyright (c) 2014 Bike Path. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GeocodeItem : NSObject
+
+@end
